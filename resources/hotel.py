89d7392e3079c3d5eaba4e11fb2fca73c8a6513d
fafreir <fabricioadriel@yahoo.com.br>
2025-04-06 09:33:24 -0300

    Criando novo hotel, utilizando reqparse

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -36,7 +36,32 @@ class Hotel(Resource):
         return {'message': 'Hotel not found.'}, 404
 
     def post(self, hotel_id):
-        pass 
+        """Recebe os elementos da requisição e habilita o parseamento"""
+        argumentos = reqparse.RequestParser()
+
+        """Irá pegar os argumentos especificos"""
+        argumentos.add_argument('nome')
+        argumentos.add_argument('estrelas')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+
+        """Irá instanciar e parsear com chave e valor"""
+        dados = argumentos.parse_args()
+
+        """Será criado um novo_hotel, iremos acessar através da instancia dados"""
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+
+        """Adiciona na lista hoteis o novo hotel"""
+        hoteis.append(novo_hotel)
+        
+        """Retornar o novo hotel com o status code de sucesso 200"""
+        return novo_hotel, 200
 
     def put(self, hotel_id):
         pass 
