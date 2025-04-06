d8ff9d87540e33e9e1a0bbd9eeeb68b5d2309739
fafreir <fabricioadriel@yahoo.com.br>
2025-04-06 10:50:08 -0300

    Implementado Delete

--- resources/hotel.py
+++ resources/hotel.py
@@ -85,4 +85,6 @@ class Hotel(Resource):
         return novo_hotel, 201
 
     def delete(self, hotel_id):
-        pass 
+        global hoteis 
+        hoteis = [hoteis for hotel in hoteis if hotel != hotel_id]
+        return {'message': 'Hotel deleted.'}
