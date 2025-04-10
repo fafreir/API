591c45ce8f026905b80f0bb55baa8094084b638c
fafreir <fabricioadriel@yahoo.com.br>
2025-04-10 06:12:13 -0300

    Atualizado rota de usuario

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_restful import Api 
 from resources.hotel import Hoteis, Hotel
+from resources.usuario import User
 import os
 
 diretorio_atual = os.path.abspath(os.path.dirname(__file__))
@@ -19,6 +20,7 @@ def cria_banco():
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User, '/usuarios/<int:user_id>')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
