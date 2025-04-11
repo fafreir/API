b2b858cf93cfd8b64cb0d6033fc0d43c4cd0143a
fafreir <fabricioadriel@yahoo.com.br>
2025-04-11 06:58:15 -0300

    Inserido POST para cadastro

--- resources/usuario.py
+++ resources/usuario.py
@@ -18,3 +18,17 @@ class User(Resource):
                 return {'message':"An internal error ocurred trying to delete hotel"}, 500
             return {'message': 'User deleted.'}
         return {'message':'User not found'}, 404
+    
+class UserRegister(Resource):
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+        dados = atributos.parse_args()
+        if UserModel.find_by_login(dados['login']):
+            return {'message':"The login '{}' already exists".format(dados['login'])}
+        # user = UserModel(dados['login'], dados['senha'])
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message':'User create sucessfully'}, 201
+    
