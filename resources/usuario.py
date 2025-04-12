906eb23c1bfee5901ad361860e86402e22198a39
fafreir <fabricioadriel@yahoo.com.br>
2025-04-12 16:06:06 -0300

    Configurado o metodo UserLogout

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
+from blocklist import BLOCKLIST
 from models.usuario import UserModel
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 import hmac
 
     
@@ -49,3 +50,11 @@ class UserLogin(Resource):
             token_de_acesso = create_access_token(identity=str(user.user_id))
             return {'access_token':token_de_acesso}, 200
         return {'message':'The username or password is incorrect.'}, 401
+    
+class UserLogout(Resource):
+
+    @jwt_required()
+    def post(self):
+        jti_id = get_jwt()['jti'] # JWT Token Identifier
+        BLOCKLIST.add(jti_id)
+        return {'message':'Logged out sucessfully!'}, 200
