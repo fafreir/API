5125a151f74c596e52906ec9ca91b3e70f19719a
fafreir <fabricioadriel@yahoo.com.br>
2025-04-12 16:04:34 -0300

    Configurado jwt

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask 
+from flask import Flask, jsonify 
 from flask_restful import Api 
+from blocklist import BLOCKLIST
 from resources.hotel import Hoteis, Hotel
 from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_caminho}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config["JWT_SECRET_KEY"] = "super-secret-key" 
+app.config["JWT_BLOCKLIST_ENABLED"] = True  # Ativa o BLOCKLIST
 api = Api(app)
 jwt = JWTManager(app)
 
@@ -19,6 +21,20 @@ jwt = JWTManager(app)
 @app.before_request
 def cria_banco():
     banco.create_all()
+
+@jwt.token_in_blocklist_loader
+def check_if_token_in_blocklist(self, token):
+    return token["jti"] in BLOCKLIST
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return  jsonify(
+            {"description": "The token has been revoked.", "error": "token_revoked"}
+        ), 401
+
+
+
        
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
