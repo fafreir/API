7a6eddd0deac0db6319296d2e7b9533764976c03
fafreir <fabricioadriel@yahoo.com.br>
2025-04-12 08:30:46 -0300

    Criado a classe UserLogin e utilizado token com o flask_jwt_extended

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,13 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+import hmac
+
     
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+
 class User(Resource):
   
     def get(self, user_id):
@@ -21,9 +28,7 @@ class User(Resource):
     
 class UserRegister(Resource):
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
+        
         dados = atributos.parse_args()
         if UserModel.find_by_login(dados['login']):
             return {'message':"The login '{}' already exists".format(dados['login'])}
@@ -31,4 +36,16 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return {'message':'User create sucessfully'}, 201
-    
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+
+        # if user and safe_str_cmp(user.senha, dados['senha'])
+        if user and hmac.compare_digest(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token':token_de_acesso}, 200
+        return {'message':'The username or password is incorrect.'}, 401
