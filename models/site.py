8b88a770930bc86d82938ad132569f4e59e3c21b
fafreir <fabricioadriel@yahoo.com.br>
2025-04-13 17:23:24 -0300

    Implementado o método find_by_id e atualizado o método delete_site

--- models/site.py
+++ models/site.py
@@ -23,11 +23,21 @@ class SiteModel(banco.Model):
         if site:
             return site
         return None 
+
+    @classmethod
+    def find_by_id(cls, site_id):
+        site = cls.query.filter_by(site_id=site_id).first() 
+        if site:
+            return site
+        return None 
     
     def save_site(self):
         banco.session.add(self)
         banco.session.commit()
 
     def delete_site(self):
+        # Deletando todos hoteis associados ao site
+        [hotel.delete_hotel() for hotel in self.hoteis]
+        # Deletando site
         banco.session.delete(self)
         banco.session.commit()
